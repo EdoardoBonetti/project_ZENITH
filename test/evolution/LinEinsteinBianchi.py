87164800c0ad62e5b05119317691795700557dad
Edoardo Bonetti <bonettiedo@gmail.com>
2023-06-13 11:52:06 +0200

    small changegs

--- test/evolution/LinEinsteinBianchi.py
+++ test/evolution/LinEinsteinBianchi.py
@@ -4,8 +4,8 @@ import sys
 from sys import path
 currentpath = os.getcwd()
 # take the current path string and take away the last folder until folder "Project_ZENITH" is found
-while currentpath.split("/")[-1] != "project_ZENITH":
-    currentpath = "~/Desktop/project_ZENITH/"
+#while currentpath.split("//")[-1] != "project_ZENITH":
+currentpath = "C:/Users/User/OneDrive/Desktop/project_ZENITH/"
 
 # add the path to the sys.path if it does not exist
 if currentpath not in sys.path: sys.path.append(currentpath)
@@ -17,71 +17,37 @@ from ngsolve import *
 from zenith import *
 
 
+
 def main():
-    # create the mesh
-    mesh = DefaultMesh(h = 0.2, R = 6)
 
-    print("Create the LinEinsteinBianchi object")
-
-    eb = LinEinsteinBianchi(mesh, order = 2)
-
-
-
-
-def Curl(u):
-    if u.dim == 3:
-        return CF( (-u[1].Diff(z)+ u[2].Diff(y), -u[2].Diff(x)+ u[0].Diff(z), -u[0].Diff(y)+ u[1].Diff(x)) )
-    if u.dim == 9:
-        return CF( (Curl(u[0,:]),Curl(u[1,:]),Curl(u[2,:])),dims=(3,3) )
-
-def Test():
-
-    kwargs = {"inverse" : "pardiso",
-              "nonassemble" : True, 
+    kwargs = {"inverse" : "sparsecholesky",
+              "nonassemble" : False, 
               "order" : 1}
 
-    # check if the curl is working
-
-
-
-
     cube = OrthoBrick( Pnt(0,0,0), Pnt(1,1,1) )
-
     geo = CSGeometry()
     geo.Add (cube)
-    mesh = Mesh(geo.GenerateMesh(maxh=0.1) )
-
-
-    n = specialcf.normal(mesh.dim)
-    Cn = CoefficientFunction( (0, n[2], -n[1], -n[2], 0, n[0], n[1], -n[0], 0) , dims=(3,3) )
-    Pn = OuterProduct(n,n)
-    Qn = Id(3) - Pn        
-    def CurlOp(u,v) : return InnerProduct(curl(u),v)*dx - InnerProduct(Pn*u*Cn,Pn*v*Qn)*dx(element_boundary = True) 
-    
-
-    Hcc = HCurlCurl(mesh, order = kwargs.get("order",2) )
-    Hcd = HCurlDiv(mesh, order = kwargs.get("order",2)  )
-
-    # test the curl of a Hcd function
-    h = exp(-x*x-y*y-z*z)
-    cf_init = CF((h,0,0 , 0,-h,0 , 0,0,0), dims=(3,3))
-    gf_init = GridFunction(Hcc)
-    gf_init.Set(cf_init)
-
-    gf_exact_curl = GridFunction(Hcd)
-    gf_exact_curl.Set(Curl(cf_init))
-
-    Draw(Curl(cf_init),mesh,"ExactCurl")
-    Draw(gf_exact_curl,mesh,"NumericalCurl")
-    Draw(Curl(cf_init)-gf_exact_curl,mesh,"ErrorCurl")
-
-    print("L2Error", Integrate(sqrt(Norm(Curl(cf_init)-gf_exact_curl)),mesh))
-
-    for i in range(3):
-        for j in range(3):
-            print("L2Error",i,j , Integrate(Curl(cf_init)[i,j]-gf_exact_curl[i,j],mesh))
+    mesh = Mesh(geo.GenerateMesh(maxh=0.3) )
 
     
+    eb = LinEinsteinBianchi(mesh, order = 2, **kwargs)
+
+    r = sqrt(x*x+y*y+z*z)
+    cf_f = exp(-r*r)
+    
+    initial_E = cf_init
+    initial_B = cf_init
+
+    # set initial values
+    print("Setting initial values")
+    eb.SetInitialValues(cf_E=initial_E, cf_B=initial_B)
+    print("Plotting")
+    eb.Plot()
+    print("Done")
+    input("Press Enter to continue...")
+    for i in range(100):
+        eb.Step(0.005)
+        eb.Plot()
 
 
 if __name__ == "__main__":
--- zenith/evolution/LinEinsteinBianchi.py
+++ zenith/evolution/LinEinsteinBianchi.py
@@ -7,7 +7,8 @@ from netgen.csg import *
 # define the class for the Einstein Bianchi equations
 class LinEinsteinBianchi:
     def __init__(self, mesh, order, **kwargs):
-        
+        self.mesh = mesh
+
         self.Hcc = HCurlCurl(mesh, order = order, dirichlet = "outer")
         self.Hcd = HCurlDiv(mesh, order = order, dirichlet = "outer")
 
@@ -22,23 +23,43 @@ class LinEinsteinBianchi:
         print("Define mass matrices: done")
 
         print("Define inverse mass matrices:", end="\r")
-        inverse = kwargs.get("inverse", "pardiso")
+        inverse = kwargs.get("inverse", "sparsecholesky")
         self.invmassHcc = self.massHcc.mat.Inverse(self.Hcc.FreeDofs(), inverse)
         self.invmassHcd = self.massHcd.mat.Inverse(self.Hcd.FreeDofs(), inverse)
         print("Define inverse mass matrices: done")
 
-        print("Define stiffness matrices:", end="\r")
-        def vol_curl(u,v):
-            return InnerProduct(curl(u), v) * dx
-        
-        
+        print("Define curl matrix:", end="\r") 
+        u = self.Hcc.TrialFunction()
+        dv = self.Hcd.TestFunction()
         n = specialcf.normal(mesh.dim)
         Cn = CoefficientFunction( (0, n[2], -n[1], -n[2], 0, n[0], n[1], -n[0], 0) , dims=(3,3) )
         Pn = OuterProduct(n,n)
         Qn = Id(3) - Pn        
         def CurlOp(u,v) : return InnerProduct(curl(u),v)*dx - InnerProduct(Pn*u*Cn,Pn*v*Qn)*dx(element_boundary = True) 
+        self.curlOp = BilinearForm(trialspace = self.Hcc, testspace = self.Hcd, nonassemble = kwargs.get("nonassemble", False))
+        self.curlOp += CurlOp(u, dv)
+        if kwargs.get("nonassemble", False) == False: self.curlOp.Assemble()
+        print("Define curl matrix: done")
 
+        self.invmasscurl = self.invmassHcc @ self.curlOp.mat.T 
 
+        self.gf_E = GridFunction(self.Hcc)
+        self.gf_B = GridFunction(self.Hcd)
+
+    def SetInitialValues(self, cf_E, cf_B):
+        self.gf_E.Set(cf_E)
+        self.gf_B.Set(cf_B)
+
+    def ApplyCurl(self, gf_out, gf_in, **kwargs):
+        gf_out.vec.data = self.invmasscurl * gf_in.vec
+
+    def Plot(self):
+        Draw(self.gf_E, self.mesh, "E")
+        Draw(self.gf_B, self.mesh, "B")
+
+    def Step(self, dt):
+        self.gf_E.vec.data -= dt * self.invmassHcc @ self.curlOp.mat.T * self.gf_B.vec
+        self.gf_B.vec.data += dt * self.invmassHcd @ self.curlOp.mat * self.gf_E.vec
 
 def Curl(u):
     if u.dim == 3:
@@ -46,51 +67,3 @@ def Curl(u):
     if u.dim == 9:
         return CF( (Curl(u[0,:]),Curl(u[1,:]),Curl(u[2,:])),dims=(3,3) )
 
-def main():
-
-    kwargs = {"inverse" : "pardiso",
-              "nonassemble" : True, 
-              "order" : 2}
-
-    # check if the curl is working
-
-
-
-
-    cube = OrthoBrick( Pnt(0,0,0), Pnt(1,1,1) )
-
-    geo = CSGeometry()
-    geo.Add (cube)
-    mesh = Mesh(geo.GenerateMesh(maxh=0.1) )
-
-
-    n = specialcf.normal(mesh.dim)
-    Cn = CoefficientFunction( (0, n[2], -n[1], -n[2], 0, n[0], n[1], -n[0], 0) , dims=(3,3) )
-    Pn = OuterProduct(n,n)
-    Qn = Id(3) - Pn        
-    def CurlOp(u,v) : return InnerProduct(curl(u),v)*dx - InnerProduct(Pn*u*Cn,Pn*v*Qn)*dx(element_boundary = True) 
-    
-
-    Hcc = HCurlCurl(mesh, order = kwargs.get("order",2)  , dirichlet = "outer")
-    Hcd = HCurlDiv(mesh, order = kwargs.get("order",2)  , dirichlet = "outer")
-
-    # test the curl of a Hcd function
-    h = exp(-x*x-y*y-z*z)
-    cf_init = CF((h,0,0 , 0,-h,0 , 0,0,0), dims=(3,3))
-    gf_init = GridFunction(Hcc)
-    gf_init.Set(cf_init)
-
-    gf_exact_curl = GridFunction(Hcc)
-    gf_exact_curl.Set(Curl(cf_init))
-
-    Draw(Curl(cf_init),mesh,"ExactCurl")
-    Draw(gf_exact_curl,mesh,"NumericalCurl")
-
-    print("L2Error", Integrate(sqrt(Norm(Curl(cf_init)-gf_exact_curl)),mesh))
-
-
-
-
-if __name__ == "__main__":
-    with TaskManager():
-        main()
